24f827855dca0d6b5dd7b1a013db7e61b2eaefd8
gecmanow <gecmanow@gmail.com>
2021-10-24 22:34:14 +0700

    edit title, add description

--- resources/views/public/layout/public.blade.php
+++ resources/views/public/layout/public.blade.php
@@ -5,7 +5,7 @@
         <title>Купить скорлупу и отводы ППУ с выгодой 20% | СК Теплострой</title>
         <meta name="description" content="Производство теплоизоляционных изделий из пенополиуретана - скорлупа и отводы ППУ в Красноярске и Челябинске"/>
         <meta name="keywords" content="скорлупа ппу, отводы ппу, купить скорлупу ппу">
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" type="image/x-icon">
         <meta name="yandex-verification" content="c87268e5ba71b290" />
         <link rel="stylesheet" href="{{ asset('/static/bootstrap/css/bootstrap.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('static/css/app.css') }}" />
