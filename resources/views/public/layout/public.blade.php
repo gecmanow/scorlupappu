f1d9ca5005dbab445ad66b3603dfc5c2da42c287
gecmanow <gecmanow@gmail.com>
2021-10-24 22:32:19 +0700

    edit title, add description

--- resources/views/public/layout/public.blade.php
+++ resources/views/public/layout/public.blade.php
@@ -1,15 +1,17 @@
-<html xmlns:xi="http://www.w3.org/2001/XInclude">
-    <head lang="ru">
-        <meta charset="utf-8" />
-        <title>Теплострой - специализированная компания</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Купить скорлупу и отводы ППУ с выгодой 20% | СК Теплострой</title>
+        <meta name="description" content="Производство теплоизоляционных изделий из пенополиуретана - скорлупа и отводы ППУ в Красноярске и Челябинске"/>
+        <meta name="keywords" content="скорлупа ппу, отводы ппу, купить скорлупу ппу">
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <meta name="yandex-verification" content="c87268e5ba71b290" />
-        <link rel="stylesheet" href="/static/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="static/css/app.css" />
-        <script src="/static/js/jquery-2.1.4.min.js"></script>
-        <script src="/static/bootstrap/js/bootstrap.js"></script>
-        <script src="/static/js/script.js"></script>
+        <link rel="stylesheet" href="{{ asset('/static/bootstrap/css/bootstrap.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('static/css/app.css') }}" />
+        <script src="{{ asset('/static/js/jquery-2.1.4.min.js') }}"></script>
+        <script src="{{ asset('/static/bootstrap/js/bootstrap.js') }}"></script>
+        <script src="{{ asset('/static/js/script.js') }}"></script>
     </head>
     <body>
     @include('public.inc.header')
